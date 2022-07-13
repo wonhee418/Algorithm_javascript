064ff7f161531c616e24511aded062cb6e2aa89c
wonhee <cheeky4@naver.com>
2022-07-14 01:35:54 +0900

    기존풀이 변경

--- codewars/8kyu/Return Negative.js
+++ codewars/8kyu/Return Negative.js
@@ -17,12 +17,7 @@
  */
 
 function makeNegative(num) {
-  // Code?
-  if (num > 0) {
-    return -num;
-  } else {
-    return num;
-  }
+  return num > 0 ? -num : num;
 }
 
 console.log(makeNegative(1));
