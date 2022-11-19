e399d390506c19fe1fbb16f8818adeca81648742
wonhee418 <cheeky4@naver.com>
2022-11-19 19:49:46 +0900

    알고리즘 문제 해결

---
+++ codewars/8kyu/Convert a Boolean to a String.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * 문제 : Implement a function which convert the given boolean value into its string representation.
+ *        (주어진 boolean 값을 문자열 표현으로 변환하는 함수를 구현합니다.)
+ * 
+ * 
+ * Note : Note: Only valid inputs will be given.
+ *        (유효한 입력만 제공됩니다.)
+ *        
+ */
+
+function booleanToString(b) {
+  return String(b)
+}
+
+
+console.log(booleanToString(true));
