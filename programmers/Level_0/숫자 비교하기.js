7cd745e036566a5e118920c2a0d33d24a735b74c
wonhee418 <cheeky4@naver.com>
2022-11-23 00:40:42 +0900

    알고리즘 문제풀이

---
+++ programmers/Level_0/숫자 비교하기.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * 문제 : 정수 num1과 num2가 매개변수로 주어집니다.
+ *        두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.
+ *        
+ * 
+ * 제한사항 : 0 ≤ num1 ≤ 10,000
+ *            0 ≤ num2 ≤ 10,000
+ * 
+ */
+
+function solution(num1, num2) {
+  return num1 === num2 ? 1 : -1;
+}
+
+console.log(solution(2, 3));
