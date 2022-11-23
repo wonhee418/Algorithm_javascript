7b9d22a62d129ffd5e5a1e06c6885dab6bc03d45
wonhee418 <cheeky4@naver.com>
2022-11-23 23:51:38 +0900

    알고리즘 문제풀이

---
+++ programmers/Level_0/배열 두배 만들기.js
@@ -0,0 +1,22 @@
+/**
+ *
+ * 문제 : 정수 배열 numbers가 매개변수로 주어집니다.
+ *        numbers의 각 원소에 두배한 원소를 가진 배열을 return하도록 solution 함수를 완성해주세요.
+ *        
+ * 
+ * 제한사항 : -10,000 ≤ numbers의 원소 ≤ 10,000
+ *            1 ≤ numbers의 길이 ≤ 1,000
+ *            
+ *            
+ * 
+ */
+
+function solution(numbers) {
+  const answer = [];
+  numbers.map((i) => {
+    answer.push(i * 2)
+  })
+  return answer;
+}
+
+console.log(solution([1, 2, 100, -99, 1, 2, 3]));
